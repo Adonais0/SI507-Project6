71dbb353ba7daac19869ed14f667e25960c1aaf4
Adonais0 <chuyao@umich.edu>
2017-11-26 01:09:08 -0500

    Made queries

--- SI507_project6.py
+++ SI507_project6.py
@@ -28,7 +28,7 @@ def setup_database():
     #FOREIGN KEY(child_id) REFERENCES child(id),
     db_cursor.execute("CREATE TABLE States(id SERIAL UNIQUE, name VARCHAR(128) PRIMARY KEY)")
 
-    db_cursor.execute("CREATE TABLE Sites(id SERIAL, name VARCHAR(128) PRIMARY KEY,type VARCHAR(128), state_id INTEGER REFERENCES States (id), location VARCHAR(255), description TEXT)")
+    db_cursor.execute("CREATE TABLE Sites(id SERIAL, name VARCHAR(128) PRIMARY KEY,type VARCHAR(128), state_id INTEGER, location VARCHAR(255), description TEXT)")
     #create table
 
     db_connection.commit()
@@ -57,23 +57,21 @@ def insert(conn, cur, table, data_dict, no_return=True):
     if not no_return:
         return cur.fetchone()
 
-def csv_to_dict_list(csvFile):
+def csv_to_dict_list(csvFile,i,s_id):
     f = csv.reader(open(csvFile,'r'))
     next(f)
-    i = 1
     list_of_dict = []
     for row in f:
         row.insert(0,i)
         i = i+1
         row[3], row[2] = row[2], row[3]
-        #row.insert(3,0)# state id to be modified later
         del row[-2]
 
         dict_of_csv = {}
         dict_of_csv['id'] = row[0]
         dict_of_csv['name'] = row[1]
         dict_of_csv['type'] = row[2]
-        #dict_of_csv['state_id'] = row[3]
+        dict_of_csv['state_id'] = s_id
         dict_of_csv['location'] = row[3]
         dict_of_csv['description'] = row[4]
         list_of_dict.append(dict_of_csv)
@@ -81,35 +79,57 @@ def csv_to_dict_list(csvFile):
 states_list = [{'id':1,'name':'arkansas'},{'id':2,'name':'michigan'},{'id':3,'name':'california'}]
 # Make sure to commit your database changes with .commit() on the database connection.
 db_connection,db_cursor = get_connection_and_cursor()#connect to the database
-setup_database()
-ar_dict_list = csv_to_dict_list('arkansas.csv')
+# setup_database()
+dict_list = []
+ar_dict_list = csv_to_dict_list('arkansas.csv',1,1)
+print(len(ar_dict_list))
+mi_dict_list = csv_to_dict_list('michigan.csv',len(ar_dict_list)+1,2)
+print(len(ar_dict_list)+len(mi_dict_list))
+ca_dict_list = csv_to_dict_list('california.csv',16,3)
+for d in ar_dict_list:
+    dict_list.append(d)
+for d in mi_dict_list:
+    dict_list.append(d)
+for d in ca_dict_list:
+    dict_list.append(d)
+
 for ar_dict in states_list:
     print(ar_dict)
     insert(db_connection, db_cursor, "states", ar_dict)
-# for ar_dict in ar_dict_list:
-#     insert(db_connection, db_cursor, "sites", ar_dict)
-
+    #successfully inserted state table
+for d in dict_list:
+    print(d['id'])
+    insert(db_connection, db_cursor, "sites", d)
 
 # Write code to be invoked here (e.g. invoking any functions you wrote above)
-# if __name__ == '__main__':
-#     #execute command line arguments
-#     command = None
-#     search_term = None
-#     if len(sys.argv) > 1:
-#         command = sys.argv[1]
-#         if len(sys.argv) > 2:
-#             search_term = sys.argv[2]
-#
-#     if command == 'setup':
-#         print('setting up the database')
-#         setup_database()
-#     elif command == 'search':
-#         print('searching', search_item)
+def execute_and_print(query, numer_of_results=1):
+    db_cursor.execute(query)
+    results = db_cursor.fetchall()
+    for r in results[:numer_of_results]:
+        print(r)
+    print('--> Result Rows:', len(results))
+    print()
+    return results
 
 
 # Write code to make queries and save data in variables here.
+all_locations = execute_and_print('SELECT "location" FROM "sites" ')
+print(all_locations)
 
+beautiful_sites = execute_and_print(""" SELECT "name" FROM "sites" WHERE "description" LIKE '%beautiful%' """)
+print(beautiful_sites)
 
+natl_lakeshores = execute_and_print(""" SELECT COUNT(*) FROM "sites" WHERE "type" LIKE 'National Lakeshore' """)
+print(natl_lakeshores)
+
+michigan_names = execute_and_print(""" SELECT ("sites"."name") FROM "sites" INNER JOIN "states" ON ("sites"."state_id" = 2)""")
+print(michigan_names)
+
+total_number_arkansas = execute_and_print("""
+SELECT COUNT(*) FROM "sites"
+  WHERE
+    state_id = 1 """)
+print(total_number_arkansas)
 
 
 
--- test.py
+++ test.py
@@ -1,44 +1,39 @@
 import json
 import csv
-def csv_to_dict_list(csvFile):
+def csv_to_dict_list(csvFile,i):
     f = csv.reader(open(csvFile,'r'))
     next(f)
-    i = 1
     list_of_dict = []
     for row in f:
         row.insert(0,i)
         i = i+1
         row[3], row[2] = row[2], row[3]
-        row.insert(3,"state_id")#to be modified later
         del row[-2]
 
         dict_of_csv = {}
         dict_of_csv['id'] = row[0]
         dict_of_csv['name'] = row[1]
         dict_of_csv['type'] = row[2]
-        dict_of_csv['state_id'] = row[3]
-        dict_of_csv['location'] = row[4]
-        dict_of_csv['description'] = row[5]
+        dict_of_csv['location'] = row[3]
+        dict_of_csv['description'] = row[4]
         list_of_dict.append(dict_of_csv)
     return list_of_dict
-print(csv_to_dict_list('arkansas.csv'))
 
-def csv_to_name_dict(csvFile):
-    f = csv.reader(open(csvFile,'r'))
-    next(f)
-    i = 1
-    list_of_dict = []
-    for row in f:
-        row.insert(0,i)
-        i = i+1
-        row[3], row[2] = row[2], row[3]
-        del row[-2]
-        dict_of_csv = {}
-        dict_of_csv['id'] = row[0]
-        dict_of_csv['name'] = row[1]
-        list_of_dict.append(dict_of_csv)
-    return list_of_dict
-print(csv_to_name_dict('arkansas.csv'))
+dict_list = []
+ar_dict_list = csv_to_dict_list('arkansas.csv',1)
+print(len(ar_dict_list))
+mi_dict_list = csv_to_dict_list('michigan.csv',len(ar_dict_list)+1)
+print(len(ar_dict_list)+len(mi_dict_list))
+ca_dict_list = csv_to_dict_list('california.csv',16)
+for d in ar_dict_list:
+    dict_list.append(d)
+for d in mi_dict_list:
+    dict_list.append(d)
+for d in ca_dict_list:
+    dict_list.append(d)
+print(dict_list)
+for a in dict_list:
+    print(a['id'])
     # if key in result:
         # implement your duplicate row handling here
 
